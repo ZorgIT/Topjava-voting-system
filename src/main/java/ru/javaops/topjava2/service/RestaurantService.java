6fb7627129068cdb2f1af5ea439734bb91158aa1
Zorgit <piligrimexe@Gmail.com>
2023-05-11 01:16:10 +0300

    St4. Implements service layer

---
+++ src/main/java/ru/javaops/topjava2/service/RestaurantService.java
@@ -0,0 +1,43 @@
+package ru.javaops.topjava2.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javaops.topjava2.model.Restaurant;
+import ru.javaops.topjava2.repository.RestaurantRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+
+    @Autowired
+    public RestaurantService(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
+    //TODO причесать CRUDs, добавить проверки, оптимизировать лишнее
+    public Optional<Restaurant> getRestaurantsById(Long id) {
+        return restaurantRepository.findById(id);
+    }
+
+    public Restaurant createRestaurant(Restaurant restaurant) {
+        return restaurantRepository.save(restaurant);
+    }
+
+    public void updateRestaurant(Restaurant restaurant) {
+        restaurantRepository.save(restaurant);
+    }
+
+    public void deleteRestaurant(Restaurant restaurant) {
+        restaurantRepository.delete(restaurant);
+    }
+
+
+}
---
+++ src/main/java/ru/javaops/topjava2/service/VoteService.java
@@ -0,0 +1,45 @@
+package ru.javaops.topjava2.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javaops.topjava2.model.User;
+import ru.javaops.topjava2.model.Vote;
+import ru.javaops.topjava2.repository.VoteRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VoteService {
+    private final VoteRepository voteRepository;
+
+    @Autowired
+    public VoteService(VoteRepository voteRepository) {
+        this.voteRepository = voteRepository;
+    }
+
+    public List<Vote> getAllVotes() {
+        return voteRepository.findAll();
+    }
+
+    public Optional<Vote> getVoteByUserAndVoteDate(User user, LocalDate voteDate) {
+        return voteRepository.findByUserIdAndDate(user.getId().longValue(), voteDate);
+    }
+
+    public List<Vote> getVotesByVoteDate(LocalDate voteDate) {
+        return voteRepository.findByVoteDate(voteDate);
+    }
+
+    public Vote createVote(Vote vote) {
+        return voteRepository.save(vote);
+    }
+
+    public void updateVote(Vote vote) {
+        voteRepository.save(vote);
+    }
+
+    public void deleteVote(Vote vote) {
+        voteRepository.delete(vote);
+    }
+}
